6a2e14da38ede48f7da902e4f17276e952dbb40b
Md Mahmudul Haque Ullash <ullash.iba@gmail.com>
2021-11-21 10:11:29 -0600

    message

--- src/main/java/veriousConcepts/gitTest1.java
+++ src/main/java/veriousConcepts/gitTest1.java
@@ -2,4 +2,14 @@ package veriousConcepts;
 
 public class gitTest1 {
 
-}
+
+		public static void main(String[] args) {
+			System.out.println("git test1");
+			System.out.println("git test2");
+			System.out.println("Modification Dones");
+			System.out.println("git test3");
+			
+
+		}
+
+	}
